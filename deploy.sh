42322bd766b7d25e0e1d1bb902a62e6b4b33eb39
yaoyao <yaoyao12306@163.com>
2015-08-16 16:04:26 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SRC_ROOT=/opt/src
+WEBSITE_ROOT=/opt/SubsNoti
+cd $WEBSITE_ROOT
+git add . -A
+NOW=`date +"%Y-%m-%d %H:%M:%S"`
+git commit -m "backup at:$NOW"
+cd $SRC_ROOT
+git pull
+rsync -a $SRC_ROOT/publish/ $WEBSITE_ROOT
+FOREVER_ROOT=$WEBSITE_ROOT forever restart -l $WEBSITE_ROOT/forever.log $WEBSITE_ROOT/app.js
