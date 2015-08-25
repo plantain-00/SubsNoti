0c2c32c727bf8765466aa7249a2aae789fb3e48d
york yao <york.yao@dragonsoftbravo.com>
2015-08-25 17:52:10 +0800

    save

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,30 @@
 
 SRC_ROOT=/opt/src
 WEBSITE_ROOT=/opt/SubsNoti
+
+# backup the website
 cd $WEBSITE_ROOT
 git add . -A
 NOW=`date +"%Y-%m-%d %H:%M:%S"`
 git commit -m "backup at:$NOW"
+
+# pull the source code
 cd $SRC_ROOT
 git pull
+
+# install
 npm --registry https://registry.npm.taobao.org install
 tsd install
+
+# compile
 tsc -m commonjs *.ts
+
+# gulp task
 gulp default
+
+# publish
 rsync -a $SRC_ROOT/node_modules/ $SRC_ROOT/publish/node_modules
 rsync -a $SRC_ROOT/publish/ $WEBSITE_ROOT
+
+# restart
 FOREVER_ROOT=$WEBSITE_ROOT forever restart -l $WEBSITE_ROOT/forever.log $WEBSITE_ROOT/app.js
