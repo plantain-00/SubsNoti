497b9b2e42b02f3b3d54e1bdfb5f9af05bc8ae09
york yao <york.yao@dragonsoftbravo.com>
2015-08-28 11:39:11 +0800

    save

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,19 @@ WEBSITE_ROOT=/opt/SubsNoti
 
 # backup the website
 cd $WEBSITE_ROOT
+echo 'Starting "backup"...'
 git add . -A
 NOW=`date +"%Y-%m-%d %H:%M:%S"`
 git commit -m "backup at:$NOW"
+echo 'Finished "backup".'
 
 # publish
+echo 'Starting "rsync"...'
 rsync -a $SRC_ROOT/node_modules/ $SRC_ROOT/publish/node_modules
 rsync -a $SRC_ROOT/publish/ $WEBSITE_ROOT
+echo 'Finished "rsync".'
 
 # restart
+echo 'Starting "restart"...'
 FOREVER_ROOT=$WEBSITE_ROOT forever restart -l $WEBSITE_ROOT/forever.log $WEBSITE_ROOT/app.js
+echo 'Finished "restart".'
--- make.sh
+++ make.sh
@@ -4,31 +4,53 @@ SRC_ROOT=/opt/src
 
 # pull the source code
 cd $SRC_ROOT
+echo 'Starting "git pull"...'
 git pull
+echo 'Finished "git pull".'
 
 # install
+echo 'Starting "npm install"...'
 npm --registry https://registry.npm.taobao.org install
+echo 'Finished "npm install".'
+echo 'Starting "tsd install"...'
 tsd install
+echo 'Finished "tsd install".'
 
 # compile gulpfile.ts
+echo 'Starting "tsc gulpfile.ts"...'
 tsc -m commonjs gulpfile.ts
+echo 'Finished "tsc gulpfile.ts".'
 
 # gulp task clean
+echo 'Starting "gulp clean"...'
 gulp clean
+echo 'Finished "gulp clean".'
 
 # compile
+echo 'Starting "tsc"...'
 tsc -m commonjs *.ts
 tsc -m commonjs ./public/scripts/*.ts
 tsc -m commonjs ./public/*.ts
 tsc -m commonjs ./tests/*.ts
+echo 'Finished "tsc".'
 
 # test
+echo 'Starting "test"...'
 mocha ./tests/
+echo 'Finished "test".'
 
 # gulp task publish
+echo 'Starting "generate document"...'
 gulp document
+echo 'Finished "generate document".'
 cd ./public/
+echo 'Starting "generate js"...'
 gulp pack-js
+echo 'Finished "generate js".'
+echo 'Starting "generate html"...'
 gulp pack-html
+echo 'Finished "generate html".'
 cd ..
+echo 'Starting "publish"...'
 gulp publish
+echo 'Finished "publish".'
