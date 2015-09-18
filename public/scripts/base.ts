34311051f3ffe553072200440dff313727ec00ca
yaoyao <yaoyao12306@163.com>
2015-09-18 22:18:33 +0800

    fix #60

--- public/scripts/base.ts
+++ public/scripts/base.ts
@@ -6,21 +6,37 @@ interface GetCurrentUserResponse extends interfaces.GetCurrentUserRespopnse,inte
 
 }
 
-export function authenticate(next:(error:Error, data:GetCurrentUserResponse)=>void) {
-    $.ajax({
-        url: "/api/current_user.json",
-        data: {},
-        cache: false,
-        success: function (data:GetCurrentUserResponse) {
-            if (data.isSuccess) {
-                vueHead.$data.loginStatus = 2;
-                vueHead.$data.currentUserName = data.name;
-                vueHead.$data.canCreateOrganization = data.canCreateOrganization;
-                next(null, data);
-            } else {
-                vueHead.$data.loginStatus = 1;
-                next(new Error(data.errorMessage), null);
+function getCurrentUser(next:(data:GetCurrentUserResponse)=>void) {
+    const loginResult = window.sessionStorage.getItem("loginResult");
+
+    if (loginResult) {
+        const data:GetCurrentUserResponse = JSON.parse(loginResult);
+        next(data);
+    } else {
+        $.ajax({
+            url: "/api/current_user.json",
+            data: {},
+            cache: false,
+            success: function (data:GetCurrentUserResponse) {
+                window.sessionStorage.setItem("loginResult", JSON.stringify(data));
+
+                next(data);
             }
+        });
+    }
+}
+
+export function authenticate(next:(error:Error, data:GetCurrentUserResponse)=>void) {
+    getCurrentUser(data=> {
+        if (data.isSuccess) {
+            vueHead.$data.loginStatus = 2;
+            vueHead.$data.currentUserName = data.name;
+            vueHead.$data.canCreateOrganization = data.canCreateOrganization;
+
+            next(null, data);
+        } else {
+            vueHead.$data.loginStatus = 1;
+            next(new Error(data.errorMessage), null);
         }
     });
 }
