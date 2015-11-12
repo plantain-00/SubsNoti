4530f1bb08f078f4e7845789e5abbbfe8364b28c
yao <yao@boqii.com>
2015-11-12 15:07:54 +0800

    clean scripts v0.8.3

--- scripts/make.sh
+++ scripts/make.sh
@@ -24,43 +24,7 @@ echo 'Starting "gulp clean"...'
 gulp clean
 echo 'Finished "gulp clean".'
 
-# compile
-echo 'Starting "tsc"...'
-tsc
-tsc -p frontends
-tsc -p backends
-echo 'Finished "tsc".'
-
-# test
-echo 'Starting "test"...'
-mocha publish/backends/tests
-echo 'Finished "test".'
-
-# gulp tasks
-echo 'Starting "css"...'
-gulp css
-echo 'Finished "css".'
-
-echo 'Starting "js"...'
-gulp js
-echo 'Finished "js".'
-
-echo 'Starting "rev"...'
-gulp rev
-echo 'Finished "rev".'
-
-echo 'Starting "html"...'
-gulp html
-echo 'Finished "html".'
-
-echo 'Starting "doc"...'
-gulp doc
-echo 'Finished "doc".'
-
-echo 'Starting "dot"...'
-gulp dot
-echo 'Finished "dot".'
-
-echo 'Starting "icon".'
-gulp icon
-echo 'Finished "icon"...'
+## gulp task make
+echo 'Starting "gulp make"...'
+gulp make
+echo 'Finished "gulp make".'
