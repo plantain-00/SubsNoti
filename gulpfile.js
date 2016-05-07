e12241cf4c23169589fc70cd1bdba88ef69d1a5e
york yao <yaoao12306@outlook.com>
2016-05-07 20:31:31 +0800

    remove gulp-shell and live-server

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 "use strict";
 
 const gulp = require("gulp");
-const shell = require("gulp-shell");
-const liveServer = require("live-server");
 const mkdirp = require("mkdirp");
 const colors = require("colors");
 
@@ -16,26 +14,4 @@ mkdirp("test_images/tmp", error => {
     if (error) {
         console.log(colors.red(error));
     }
-});
-
-gulp.task("host-imageServer", () => {
-    liveServer.start({
-        port: 7777,
-        host: "0.0.0.0",
-        root: "images",
-        open: false,
-        ignore: "",
-        wait: 500,
-    });
-});
-
-gulp.task("host-imageServer-test", () => {
-    liveServer.start({
-        port: 7777,
-        host: "0.0.0.0",
-        root: "test_images",
-        open: false,
-        ignore: "",
-        wait: 500,
-    });
 });
